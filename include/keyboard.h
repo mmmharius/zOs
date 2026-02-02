af5a71568da36a65672a3e23d90e7a9134340ba3
Marius <mariuspapin@rcmm.fr>
2026-02-02 16:08:37 +0100

    keyboard handler done but weird print

---
+++ include/keyboard.h
@@ -0,0 +1,36 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#define KB_DATA 0x60
+#define KB_STATUS 0x64
+
+#define KEY_ENTER 0x1C
+#define KEY_BACKSPACE 0x0E
+#define KEY_Q 0x10
+#define KEY_W 0x11
+#define KEY_E 0x12
+#define KEY_R 0x13
+#define KEY_T 0x14
+#define KEY_Y 0x15
+#define KEY_U 0x16
+#define KEY_I 0x17
+#define KEY_O 0x18
+#define KEY_P 0x19
+#define KEY_A 0x1E
+#define KEY_S 0x1F
+#define KEY_D 0x20
+#define KEY_F 0x21
+#define KEY_G 0x22
+#define KEY_H 0x23
+#define KEY_J 0x24
+#define KEY_K 0x25
+#define KEY_L 0x26
+#define KEY_Z 0x2C
+#define KEY_X 0x2D
+#define KEY_C 0x2E
+#define KEY_V 0x2F
+#define KEY_B 0x30
+#define KEY_N 0x31
+#define KEY_M 0x32
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "keyboard.h"
 #include "libasm/libasm.h"
 #include <sys/io.h>
 
@@ -19,6 +20,40 @@ unsigned char read_keyboard() {
         return inb(KB_DATA);
 }
 
+char scancode_to_ascii(unsigned char sc) {
+    switch(sc) {
+        case KEY_Q: return 'Q';
+        case KEY_W: return 'W';
+        case KEY_E: return 'E';
+        case KEY_R: return 'R';
+        case KEY_T: return 'T';
+        case KEY_Y: return 'Y';
+        case KEY_U: return 'U';
+        case KEY_I: return 'I';
+        case KEY_O: return 'O';
+        case KEY_P: return 'P';
+        case KEY_A: return 'A';
+        case KEY_S: return 'S';
+        case KEY_D: return 'D';
+        case KEY_F: return 'F';
+        case KEY_G: return 'G';
+        case KEY_H: return 'H';
+        case KEY_J: return 'J';
+        case KEY_K: return 'K';
+        case KEY_L: return 'L';
+        case KEY_Z: return 'Z';
+        case KEY_X: return 'X';
+        case KEY_C: return 'C';
+        case KEY_V: return 'V';
+        case KEY_B: return 'B';
+        case KEY_N: return 'N';
+        case KEY_M: return 'M';
+        case KEY_ENTER: return '\n';
+        case KEY_BACKSPACE: return '\b';
+        default: return '?';
+    }
+}
+
 void    print_char(char c) {
     volatile uint16_t* vga = (uint16_t*)0xB8000;
     if (LIN == 80) {
@@ -28,6 +63,16 @@ void    print_char(char c) {
     vga[LIN + COL * 80] = (uint16_t)c | 0x0F00;
     LIN++;
 }
+
+void keyboard_loop() {
+    while(1) {
+        unsigned char sc = read_keyboard();
+        char c = scancode_to_ascii(sc);
+        print_char(c);
+    }
+}
+
+
 
 
 /*
@@ -56,5 +101,5 @@ void    print_char(char c) {
 
 void main() {
     // print_42();
-    print_char(read_keyboard());
+    keyboard_loop();
 }
